7d6a2f3f2b6dd0cdb7506c6df399d1c4d36df8dc
aadhya <aadhya@umich.edu>
2024-03-31 18:17:15 -0400

    1.0: UI partially done. Supabase API Call to Groups works

--- supabase.js
+++ supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-//const supabaseUrl = process.env.SUPABASE_URL
-//const supabaseKey = process.env.SUPABASE_KEY
-const SUPABASE_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpZ253cW5tdnByeWFzZ3dmdWhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQwMDc0MTAsImV4cCI6MjAxOTU4MzQxMH0.DDjkGG4M4od416fvjRSmxkRJaF0p4llOhAe032wiBug'
-const SUPABASE_URL= 'https://yignwqnmvpryasgwfuhn.supabase.co'
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
 export default supabase
